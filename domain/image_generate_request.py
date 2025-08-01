6ce8e12d65c6526fde68825a9d29d19dfe4356f9
Igor Djuric <idjuric@tenstorrent.com>
2025-08-01 13:32:52 +0000

    Removed unused objects & tests

--- domain/image_generate_request.py
+++ domain/image_generate_request.py
@@ -1,11 +1,9 @@
 from typing import Optional
 from pydantic import BaseModel, Field, PrivateAttr
 
-# from domain.output_format import OutputFormat
-
 class ImageGenerateRequest(BaseModel):
     prompt: str
-    negative_prompt: Optional[str] = None
+    # negative_prompt: Optional[str] = None
     # output_format: OutputFormat
-    num_inference_step: int = Field(..., ge=1, le=50)
+    # num_inference_step: int = Field(..., ge=1, le=50)
     _task_id: str = PrivateAttr()
